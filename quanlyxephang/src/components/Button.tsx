77d029835dcbf387fb9dc0c969341e6a66e2b16c
Nguyen Hau <hakuaeri@gmail.com>
2023-04-26 10:19:01 +0700

    component button

--- quanlyxephang/src/components/Button.tsx
+++ quanlyxephang/src/components/Button.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
-export const Button = () => {
-  return (
-    <div>Button</div>
-  )
+import React from "react";
+type btProps={
+  text: string
 }
+
+const Button1 = (props: btProps) => {
+  return (
+    <button className="button-bg">
+      <p className="button-color">{props.text}</p>
+    </button>
+  );
+};
+
+export default Button1;
--- quanlyxephang/src/index.tsx
+++ quanlyxephang/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Login } from './pages/Account/Login';
 import Logoalta from './components/Logo';
 import './styles/Logo.css'
+import Button1 from './components/Button';
+import './styles/Button.css'
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -10,6 +14,6 @@ root.render(
   <div>
     
     <Logoalta></Logoalta>
-    
+    <Button1 text='Đăng nhập'/>
   </div>
 )
