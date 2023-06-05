dd3b313157dc09b0c8154687f5d03882c99e0320
Nguyen Hau <hakuaeri@gmail.com>
2023-06-05 13:57:53 +0700

    add

--- quanlyxephang/src/components/ThietBi/AddThietBi/AddThietBi.tsx
+++ quanlyxephang/src/components/ThietBi/AddThietBi/AddThietBi.tsx
@@ -5,23 +5,33 @@ export const AddThietBi = () => {
   return (
     <div className='container-ip'>
       <div className='left'>
-        <Input
-          text={''}
-          label={'Mã thiết bị:*'}
-          placeholder={'Nhập mã thiết bị'}
-          onChange={undefined}
-          style={{
-            width: '540px',
-            height: '44px',
-            border: '1.5px solid #D4D4D7',
-            borderRadius: '8px',
-            fontSize: '16px',
-            padding: '10px 270px 10px 12px',
-          }}
-        ></Input>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Input
+                  text={''}
+                  label={'Mã thiết bị:*'}
+                  placeholder={'Nhập mã thiết bị'}
+                  onChange={undefined}
+                  style={{
+                    width: '520px',
+                    height: '44px',
+                    border: '1.5px solid #D4D4D7',
+                    borderRadius: '8px',
+                    fontSize: '16px',
+                    padding: '10px 20px',
+                  }}
+                />
+              </td>
+              <td>báds</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+
       <div className='right'>
-        
+
       </div>
 
       <div className='footer'>
